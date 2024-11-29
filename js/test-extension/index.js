8efeea7cafed4b6e625fb06e229d68e43b2daec4
Seba Maldonado <sebamza_17@hotmail.com>
2024-11-29 10:48:58 -0300

    feat/test-extension: adds simple test extension (trigger build)

---
+++ js/test-extension/index.js
@@ -0,0 +1,20 @@
+/**
+ * DO NOT REMOVE THIS, we're using this example for our test suite
+ * on "unit/services/extensions/extension-handler-test.js"
+ */
+export default function run () {
+  const accountNameOnTopBar = document.querySelector('[data-test-authenticated-account-name]')
+
+  if (accountNameOnTopBar) {
+    // change background color
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    accountNameOnTopBar.style.backgroundColor = 'red'
+
+    // change innerHTML
+    accountNameOnTopBar.innerHTML = 'EXTENSION EXAMPLE TEXT'
+
+    // finally, we add an attribute to know that DOM was changed by an extensions
+    accountNameOnTopBar.setAttribute('data-extension-updated', 'true')
+  }
+}
